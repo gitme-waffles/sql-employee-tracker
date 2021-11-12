019be54c3d745b6fbd5f2a732af54c87e87e00fa
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-11-12 23:14:13 +1100

    added view all employees to query file

--- db/query.sql
+++ db/query.sql
@@ -20,11 +20,21 @@ FROM employee AS E
 INNER JOIN role AS R ON R.id = E.role_id
 INNER JOIN department AS D ON  R.department_id = D.id;
 
+
 -- SELECT *
 --     `name` AS `Dept.`, id AS ID
 -- FROM department
 
-SELECT
-    E.first_name AS Employee, M.first_name AS Manager
-FROM employee AS E 
-INNER JOIN manager AS M on M.id = E.manager_id
+                    -- VIEW ALL EMPLOYEES
+SELECT 
+  E.id AS "ID", CONCAT(E.first_name, " ", E.last_name) AS Employee, R.title AS "Job Title", D.name AS Department, R.salary AS Salary, CONCAT(EMP.first_name, " ", EMP.last_name) AS Manager
+FROM employee AS E
+INNER JOIN role AS R ON R.id = E.role_id
+INNER JOIN department AS D ON  R.department_id = D.id
+LEFT JOIN employee AS EMP ON EMP.id = E.manager_id;
+
+            -- *** MOCK UP ***
+-- SELECT
+--     E.first_name AS Employee, M.first_name AS Manager
+-- FROM employee AS E 
+-- INNER JOIN manager AS M on M.id = E.manager_id

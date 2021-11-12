c220c73ae501cbc37f7e7469f3b994664a24d498
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-11-12 21:25:31 +1100

    base seeds added

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (`name`)
+VALUES  ("Sales");,
+        ("Engineering"),
+        ("Finance"),
+        ("Legal");
+
+INSERT INTO `role` (title, salary, department_id)
+VALUES  ("Sales Lead", 100000, 1),
+        ("Salesperson", 80000, 1),
+        ("Lead Engineet", 150000, 2),
+        ("Software Engineer", 120000, 2),
+        ("Account Manager", 160000, 3),
+        ("Accountant", 125000, 3),
+        ("Legal Team Lead", 250000, 4),
+        ("Lawyer", 200000, 4);
+
+INSERT INTO employee (first_name, last_name, role_id)  
+VALUES  ("John", "Smith", 1),
+        ("Mike", "Chan", 2),
+        ("Hugh", "Mungus", 3),
+        ("Paul", "Mcartney", 4),
+        ("George", "Harrison", 5),
+        ("John", "Lemon", 6),
+        ("Ringo", "Star", 7),
+        ("Deez", "Nughts", 8);

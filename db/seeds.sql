d05db1b7e60e57054c897e5e67484ef9729d6dd1
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-11-13 21:02:18 +1100

    typo fix

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,7 +7,7 @@ VALUES  ("Sales"),
 INSERT INTO `role` (title, salary, department_id)
 VALUES  ("Sales Lead", 100000, 1),
         ("Salesperson", 80000, 1),
-        ("Lead Engineet", 150000, 2),
+        ("Lead Engineer", 150000, 2),
         ("Software Engineer", 120000, 2),
         ("Account Manager", 160000, 3),
         ("Accountant", 125000, 3),

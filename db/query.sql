d1d8bf3af22a059a04e884b64894661157c76fea
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-11-14 19:49:14 +1100

    modify rquest job title and manager data, added update query

--- db/query.sql
+++ db/query.sql
@@ -9,16 +9,17 @@
 -- INNER JOIN `role` AS R ON E.role_id = R.id
 -- INNER JOIN department AS D ON R.department_id = D.id;
 
+            --  *** View All Role Details ***
 -- SELECT 
 --   R.id AS "ID", R.title AS "Job Title", D.name AS "Dept.", R.salary AS Salary
 -- FROM role AS R
 -- INNER JOIN department AS D ON  R.department_id = D.id;
 
-SELECT 
-  E.id AS "ID", CONCAT(E.first_name, " ", E.last_name) AS Employee, R.title AS "Job Title", R.id AS Job_ID, D.name AS "Dept.", R.salary AS Salary, CONCAT(E.first_name, " ", E.last_name) AS Manager
-FROM employee AS E
-INNER JOIN role AS R ON R.id = E.role_id
-INNER JOIN department AS D ON  R.department_id = D.id;
+-- SELECT 
+--   E.id AS "ID", CONCAT(E.first_name, " ", E.last_name) AS Employee, R.title AS "Job Title", R.id AS Job_ID, D.name AS "Dept.", R.salary AS Salary, CONCAT(E.first_name, " ", E.last_name) AS Manager
+-- FROM employee AS E
+-- INNER JOIN role AS R ON R.id = E.role_id
+-- INNER JOIN department AS D ON  R.department_id = D.id;
 
 
 -- SELECT *
@@ -26,12 +27,12 @@ INNER JOIN department AS D ON  R.department_id = D.id;
 -- FROM department
 
                     -- *** VIEW ALL EMPLOYEES ***
-SELECT 
-  E.id AS "ID", CONCAT(E.first_name, " ", E.last_name) AS Employee, R.title AS "Job Title", D.name AS Department, R.salary AS Salary, CONCAT(EMP.first_name, " ", EMP.last_name) AS Manager
-FROM employee AS E
-INNER JOIN role AS R ON R.id = E.role_id
-INNER JOIN department AS D ON  R.department_id = D.id
-LEFT JOIN employee AS EMP ON EMP.id = E.manager_id;
+-- SELECT 
+--   E.id AS "ID", CONCAT(E.first_name, " ", E.last_name) AS Employee, R.title AS "Job Title", D.name AS Department, R.salary AS Salary, CONCAT(EMP.first_name, " ", EMP.last_name) AS Manager
+-- FROM employee AS E
+-- INNER JOIN role AS R ON R.id = E.role_id
+-- INNER JOIN department AS D ON  R.department_id = D.id
+-- LEFT JOIN employee AS EMP ON EMP.id = E.manager_id;
 
             -- *** MOCK UP ***
 -- SELECT
@@ -39,3 +40,13 @@ LEFT JOIN employee AS EMP ON EMP.id = E.manager_id;
 -- FROM employee AS E 
 -- INNER JOIN manager AS M on M.id = E.manager_id
 
+              -- *** Request Job Title and Manager data ***
+-- SELECT 
+--  R.id AS id,  R.title AS title, CONCAT(EMP.first_name, " ", EMP.last_name) AS Manager, EMP.id AS Managers_ID
+-- FROM employee AS E
+-- RIGHT JOIN role AS R ON R.id = E.role_id
+-- LEFT JOIN employee AS EMP ON EMP.id = E.manager_id;
+
+UPDATE [table]
+SET [key] = "Candide"
+WHERE id = 2;

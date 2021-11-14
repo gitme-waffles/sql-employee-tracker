ab850a7739ef7ba2602869b37987b6d8dbfa4be8
dossAnania <85494162+dossAnania@users.noreply.github.com>
2021-11-14 19:48:35 +1100

    moved mock table

--- db/schema.sql
+++ db/schema.sql
@@ -18,19 +18,6 @@ CREATE TABLE `role` (
     ON DELETE SET NULL
 );
 
-
---              *** MOCK TABLE ***
--- CREATE TABLE manager (
---     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
---     first_name VARCHAR(30) NOT NULL,
---     last_name VARCHAR(30) NOT NULL,
---     role_id INT,
---     FOREIGN KEY (role_id)
---     REFERENCES `role`(id)
---     ON DELETE SET NULL,
-
--- )
-
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
@@ -44,3 +31,15 @@ CREATE TABLE employee (
     REFERENCES employee(id)
     ON DELETE SET NULL
 );
+
+
+--              *** MOCK TABLE ***
+-- CREATE TABLE manager (
+--     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+--     first_name VARCHAR(30) NOT NULL,
+--     last_name VARCHAR(30) NOT NULL,
+--     role_id INT,
+--     FOREIGN KEY (role_id)
+--     REFERENCES `role`(id)
+--     ON DELETE SET NULL,
+-- )
